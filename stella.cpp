e9d14a9be3d3990eaf1ea66b0fbf336a7bb36c2f
Flavio Bizzarri <newfla@hotmail.it>
2018-10-17 18:39:29 +0200

    esercizio stella base ok

--- stella.cpp
+++ stella.cpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <time.h>
 
-
+float move_x=0,move_y=0, scaling=1,angle=0;
 void redraw(){
     float raggio=0.55, raggio2=0.12, temp=raggio;
     float radians=3.14/180;
@@ -14,7 +14,6 @@ void redraw(){
         glColor3f(1,1,1);
         glVertex2d(0.0,0.0);
         for(int i=0; i<=396; i+=36){
-            printf("%d\n",i);
             if(temp==raggio)
                 temp=raggio2;
             else
@@ -25,32 +24,64 @@ void redraw(){
     glFlush();
 }
 
-void keyListener(int key, int x, int y){
+void specialKeyListener(int key, int x, int y){
     switch(key){
         case(GLUT_KEY_LEFT):
-            //Traslazione
+            move_x-=0.1;
+            glTranslatef(move_x,0,0);
             break;
         case (GLUT_KEY_UP):
-            //Rotazione
+            move_y+=0.1;
+            glTranslatef(0,move_y,0);
             break;
         case(GLUT_KEY_RIGHT):
-            //Scale in grande
+            move_x+=0.1;
+            glTranslatef(move_x,0,0);
             break;
         case(GLUT_KEY_DOWN):
-            //Scale in piccolo
+            move_y-=0.1;
+            glTranslatef(0,move_y,0);
             break;
+        default:
+            printf("444");
     }
     glutPostRedisplay();
+}
+
+void keyListener(unsigned char key, int x, int y){
+        switch(key){
+        case 'w':
+            scaling+=0.1;
+            glScalef(scaling,scaling,0);
+            break;
+
+        case 's':
+            scaling-=0.1;
+            glScalef(scaling,scaling,0);
+            break;
+
+        case 'a':
+            angle+=2;
+            glRotatef(angle,0,0,1);
+            break;
+
+        case 'd':
+            angle-=2;
+            glRotatef(angle,0,0,1);
+            break;
+        }
+        glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowPosition(300,100);
-    glutInitWindowSize(500,500);
+    glutInitWindowSize(800,800);
     glutCreateWindow("");
     glutDisplayFunc(redraw);
-    glutSpecialFunc(keyListener);
+    glutSpecialFunc(specialKeyListener);
+    glutKeyboardFunc(keyListener);
     glutMainLoop();
     return 0;
 }
